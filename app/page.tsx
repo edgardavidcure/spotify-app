d3850943a40884c4a8001c2c4225a91f10d981e4
edgar <edgardavid1503@gmail.com>
2025-02-20 14:01:52 -0800

    test to fix refresh token issue

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home({
   const session = await auth();
 
   if (session?.error === "RefreshTokenRevoked") {
-    signOut();
+    () => signOut();
   }
 
   if (!session) {
